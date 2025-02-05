02ba6ff61060c7143f06b5e45e7df80c4fa6ca34
Bianca Bech <biancapaulabech@gmail.com>
2025-02-04 21:38:35 -0300

    Adicionando codigo tabuada

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calcular();
+        }
+
+        static void Calcular()
+        {
+            Console.WriteLine("Digite qual é a tabuada que você que: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero* i}"); 
+            }
+        }
+    }
+}
